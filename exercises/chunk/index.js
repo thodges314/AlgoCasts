6cea88a2ca56ad6c732b610d3bfc92b684c8a679
Thomas Hodges <thodges@javascriptonline.org>
2024-08-07 21:01:36 -0700

    chunk my simple version

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,12 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const returnValues = [];
+  for (let j = 0; j < array.length; j += size) {
+    returnValues.push(array.slice(j, j + size));
+  }
+  return returnValues;
+}
 
 module.exports = chunk;
